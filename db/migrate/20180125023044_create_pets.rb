3fc7aa95bc2ddfea1208fcfd9d0b3a578c1f6b41
TravisA <github email address>
2018-01-24 19:43:03 -0700

    create pets and parks migration

--- db/migrate/20180125023044_create_pets.rb
+++ db/migrate/20180125023044_create_pets.rb
@@ -1,4 +1,11 @@
 class CreatePets < ActiveRecord::Migration[5.1]
   def change
+  	create_table :pets do |t|
+  		t.string :name
+  		t.string :color
+  		t.string :age
+  		t.integer :owner_id
+  		t.integer :park_id
+  	end
   end
 end
--- db/migrate/20180125023051_create_parks.rb
+++ db/migrate/20180125023051_create_parks.rb
@@ -1,4 +1,8 @@
 class CreateParks < ActiveRecord::Migration[5.1]
   def change
+  	create_table :parks do |t|
+  		t.string :name
+  		t.string :location
+  	end
   end
 end
