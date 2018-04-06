c70f47621cf419c680349fa9ac9176983ec288b8
TravisA <github email address>
2018-04-06 11:30:19 -0600

    work toward postgres swap

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,6 @@ Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.establish_connection(
 	ENV['DATABASE_URL'] || 'postgres://localhost/mydb'
-	# :adapter => "postgresql",
-	# :database => "#{ENV['SINATRA_ENV']}",
-	# :host => "localhost",
-	# :username => "testuser",
-	# :password => "test123"
 )
 
 require_all 'app'
