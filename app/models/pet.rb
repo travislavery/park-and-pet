d961a4faed8d4d4db7eed6825f9929a9d526dfac
TravisA <github email address>
2018-04-07 15:25:20 -0600

    add comments to pet

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,6 +2,7 @@ class Pet < ActiveRecord::Base
 	validates_presence_of :name, :color, :breed, :age
 	belongs_to :owner
 	belongs_to :park
+  has_many :comments
 	include Slugify::InstanceMethods
 	extend Slugify::ClassMethods
   extend NameSupplier
